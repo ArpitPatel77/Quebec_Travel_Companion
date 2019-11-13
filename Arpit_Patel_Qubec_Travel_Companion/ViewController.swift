0c17acd3acbb6e5fa2ca4639b97e886c2890ca93
Arpit Patel <200393479@student.georgianc.on.ca>
2019-11-13 15:38:27 -0500

    Initial Commit

---
+++ Arpit_Patel_Qubec_Travel_Companion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arpit_Patel_Qubec_Travel_Companion
+//
+//  Created by Arpit Kamalkumar Patel on 2019-11-13.
+//  Copyright © 2019 Arpit Kamalkumar Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
